c18e29fd3f4562a614051224628921dd9225be25
Nicolas de gheselle <nicolas@degheselle.fr>
2026-07-21 11:37:36 +0200

    Add soft and outline

--- Joufflu.Samples/Views/Natives/Actions/ButtonSamples.xaml.vm.cs
+++ Joufflu.Samples/Views/Natives/Actions/ButtonSamples.xaml.vm.cs
@@ -12,6 +12,22 @@ public class ButtonSamplesViewModel : ObservableObject
         "<Button Style=\"{StaticResource Success}\">Success</Button>\n" +
         "<Button Style=\"{StaticResource Danger}\">Danger</Button>";
 
+    public string SoftCode =>
+        "<!-- Tinted background, semantic hue as text -->\n" +
+        "<Button Style=\"{StaticResource SoftPrimary}\">Primary</Button>\n" +
+        "<Button Style=\"{StaticResource SoftSuccess}\">Success</Button>\n" +
+        "<Button Style=\"{StaticResource SoftInfo}\">Info</Button>\n" +
+        "<Button Style=\"{StaticResource SoftWarning}\">Warning</Button>\n" +
+        "<Button Style=\"{StaticResource SoftDanger}\">Danger</Button>";
+
+    public string OutlineCode =>
+        "<!-- Coloured border and text, transparent fill -->\n" +
+        "<Button Style=\"{StaticResource OutlinePrimary}\">Primary</Button>\n" +
+        "<Button Style=\"{StaticResource OutlineSuccess}\">Success</Button>\n" +
+        "<Button Style=\"{StaticResource OutlineInfo}\">Info</Button>\n" +
+        "<Button Style=\"{StaticResource OutlineWarning}\">Warning</Button>\n" +
+        "<Button Style=\"{StaticResource OutlineDanger}\">Danger</Button>";
+
     public string IconCode =>
         "<Button joufflu:ControlProperties.IsSquare=\"True\">\n" +
         "    <fonts:FontIcon Text=\"{x:Static fonts:LucideFontIcons.Plus}\" />\n" +
